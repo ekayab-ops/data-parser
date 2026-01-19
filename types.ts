1968e6f35a222680b387c845b72dc28b3b217307
ekayab-ops <262583022+ekayab-ops@users.noreply.github.com>
2026-01-19 10:30:55 +0000

    Added HTTP methods enum

--- types.ts
+++ types.ts
@@ -1,9 +1,10 @@
-
 export enum HttpMethod {
   GET = 'GET',
   POST = 'POST',
   PUT = 'PUT',
   DELETE = 'DELETE',
-  PATCH = 'PATCH'
+  PATCH = 'PATCH',
+  HEAD = 'HEAD',
+  OPTIONS = 'OPTIONS',
+  CONNECT = 'CONNECT'
 }
-
